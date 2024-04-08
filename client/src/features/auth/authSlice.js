a5e3794d930cd9c26355f71d444bfb5407da4254
Vishal Vishwakarma <vishalvish4225@gmail.com>
2024-04-08 22:04:33 +0530

    Server Setup

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -11,9 +11,14 @@ export const createUserAsync = createAsyncThunk(
 
 export const checkUserAsync = createAsyncThunk(
   'user/checkUser',
-  async (loginInfo) => {
-    const response = await checkUser(loginInfo);
-    return response.data;
+  async (loginInfo, {rejectWithValue}) => {
+    try {
+      const response = await checkUser(loginInfo);
+      return response.data;
+    } catch (error) {
+      // console.log(error);
+      return rejectWithValue(error);
+    }
   },
 );
 export const updateUserAsync = createAsyncThunk(
@@ -61,7 +66,7 @@ export const authSlice = createSlice({
       })
       .addCase(checkUserAsync.rejected, (state, action) => {
         state.status = 'fulfilled';
-        state.error = action.error;
+        state.error = action.payload;
       })
       .addCase(updateUserAsync.pending, (state) => {
         state.status = 'pending';
