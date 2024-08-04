28e86975bd578c2586f28ef52b0d014ea5119403
Vishal Vishwakarma <vishalvish4225@gmail.com>
2024-08-04 20:35:38 +0530

    update api

--- client/src/features/api.js
+++ client/src/features/api.js
@@ -1,2 +1,2 @@
-export const baseURL = 'http://localhost:8080';
-// const baseURL = 'https://mern-stact-backend.onrender.com';
+// export const baseURL = 'http://localhost:8080';
+export const baseURL = 'https://mern-stact-backend.onrender.com';
